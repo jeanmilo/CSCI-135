0bbb741eee33db8a8b061e663a85baddf1eee8ad
Claudia (Dia) <claudia.parlee@gmail.com>
2025-03-13 13:23:36 -0400

    Add files via upload

---
+++ Labs/Lab 8/invert-half.cpp
@@ -0,0 +1,100 @@
+/*
+Name: Claudia Yamamoto
+Date: Apr 11
+Prof: Tong Yi
+Assignment: Lab 8A
+
+this program will invert colors of an image.
+*/
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+
+
+using namespace std;
+
+const int MAX_H = 512;
+const int MAX_W = 512;
+
+//given by prof yi
+void readImage(int image[MAX_H][MAX_W], int &height, int &width) {
+        char c;
+        int x;
+        ifstream instr;
+        instr.open("inImage.pgm");
+
+        // read the header P2
+        instr >> c;
+        assert(c == 'P');
+        instr >> c;
+        assert(c == '2');
+
+        // skip the comments (if any)
+        while ((instr>>ws).peek() == '#') {
+                instr.ignore(4096, '\n');
+        }
+
+        instr >> width;
+        instr >> height;
+
+        assert(width <= MAX_W);
+        assert(height <= MAX_H);
+        int max;
+        instr >> max;
+        assert(max == 255);
+
+        for (int row = 0; row < height; row++)
+                for (int col = 0; col < width; col++)
+                        instr >> image[row][col];
+        instr.close();
+        return;
+}
+//written by prof yi
+void writeImage(int image[MAX_H][MAX_W], int height, int width) {
+        ofstream ostr;
+        ostr.open("outImage.pgm");
+        if (ostr.fail()) {
+                cout << "Unable to write file\n";
+                exit(1);
+        };
+
+        // print the header
+        ostr << "P2" << endl;
+        // width, height
+        ostr << width << ' ';
+        ostr << height << endl;
+        ostr << 255 << endl;
+
+        for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                        assert(image[row][col] < 256);
+                        assert(image[row][col] >= 0);
+                        ostr << image[row][col] << ' ';
+                        ostr << endl;
+                }
+        }
+        ostr.close();
+        return;
+}
+
+int main(){
+
+        int img[MAX_H][MAX_W];
+        int h, w;
+
+        readImage(img, h, w);
+        int out[MAX_H][MAX_W];
+
+        for(int row = 0; row<h; row++){
+                for(int col = 0; col<w; col++){
+			if(col >= w/2) out[row][col] = (255 - img[row][col]);
+			else out[row][col] = img[row][col];
+                }
+        }
+        writeImage(out, h, w);
+
+}
+
+
---
+++ Labs/Lab 8/pixelate.cpp
@@ -0,0 +1,102 @@
+/*
+Name: Claudia Yamamoto
+Date: Apr 11
+Prof: Tong Yi
+Assignment: Lab 8D
+
+this program will create a frame around a picture that is one pixel thick. 
+*/
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+
+
+using namespace std;
+
+const int MAX_H = 512;
+const int MAX_W = 512;
+
+//given by prof yi
+void readImage(int image[MAX_H][MAX_W], int &height, int &width) {
+        char c;
+        int x;
+        ifstream instr;
+        instr.open("inImage.pgm");
+
+        // read the header P2
+        instr >> c;
+        assert(c == 'P');
+        instr >> c;
+        assert(c == '2');
+
+        // skip the comments (if any)
+        while ((instr>>ws).peek() == '#') {
+                instr.ignore(4096, '\n');
+        }
+
+        instr >> width;
+        instr >> height;
+
+        assert(width <= MAX_W);
+        assert(height <= MAX_H);
+        int max;
+        instr >> max;
+        assert(max == 255);
+
+        for (int row = 0; row < height; row++)
+                for (int col = 0; col < width; col++)
+                        instr >> image[row][col];
+        instr.close();
+        return;
+}
+//written by prof yi
+void writeImage(int image[MAX_H][MAX_W], int height, int width) {
+        ofstream ostr;
+        ostr.open("outImage.pgm");
+        if (ostr.fail()) {
+                cout << "Unable to write file\n";
+                exit(1);
+        };
+
+        // print the header
+        ostr << "P2" << endl;
+        // width, height
+        ostr << width << ' ';
+        ostr << height << endl;
+        ostr << 255 << endl;
+
+        for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                        assert(image[row][col] < 256);
+                        assert(image[row][col] >= 0);
+                        ostr << image[row][col] << ' ';
+                        ostr << endl;
+                }
+        }
+        ostr.close();
+        return;
+}
+
+int main(){
+
+        int img[MAX_H][MAX_W];
+        int h, w;
+
+        readImage(img, h, w);
+        int out[MAX_H][MAX_W];
+
+		for (int row = 0; row < h; row+=2)
+			for (int col = 0; col < w; col+=2){
+		    	int averageColor = (img[row][col] + img[row+1][col+1] + img[row+1][col] + img[row][col+1]) / 4;
+
+		    	out[row][col] = averageColor;
+		    	out[row+1][col+1] = averageColor;
+		    	out[row][col+1] = averageColor;
+		    	out[row+1][col] = averageColor;
+			}
+
+		writeImage(out, h, w);
+}
+
